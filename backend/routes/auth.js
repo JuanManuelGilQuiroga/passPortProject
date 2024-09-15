0f50ad4cffdeda6dd6334217d4f29a977fdecd7e
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-09-14 22:34:08 -0500

    feat: :sparkles: Discord strategy implementation, progressing routes progression and mongoos implemented

---
+++ backend/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const passport = require("passport");
+
+const router = express.Router();
+
+router.get('/discord', passport.authenticate('discord'));
+router.get('/discord/callback', passport.authenticate('discord', { failureRedirect: '/' }), (req, res) => {
+  res.redirect('/profile');
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
