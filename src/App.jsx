efde9dca66b9e4b0bbbd1f032bd9ee988ed3db96
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-09-15 19:57:32 -0500

    feat: :lipstick: Improved UI, and changed buttons logic
    Now we have "onClick" per each button, this onclick will open its respective auth link, to discord, facebook and google too, and also improved the whole styles to the page ( exposed to changes )

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
-import './App.css'
-import { ButtonLogIn } from './components/button'
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
 
 function App() {
-  const google = () => {
-    window.open("http://localhost:3000/auth/google", "_self")
-  }
-
-  const facebook = () => {
-    window.open("http://localhost:3000/auth/facebook", "_self")
-  }
-
-  const discord = () => {
-    window.open("http://localhost:3000/auth/discord", "_self")
-  }
-
   return (
-    <>
-      <main>
-        <ButtonLogIn name="Google" color="#E81E1E" logIn={google}/>
-        <ButtonLogIn name="Discord" color="#5865f2" logIn={discord}/>
-        <ButtonLogIn name="Facebook" color="#38569e" logIn={facebook}/>
-      </main>
-    </>
-  )
+    <main>
+      <div className="root">
+        <Form />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
